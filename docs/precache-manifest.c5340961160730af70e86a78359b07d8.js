42e01976cb1b51202daf9a44be56e9d64bce412d
xuzhidan <xuzhidan@oppo.com>
2019-04-19 17:14:27 +0800

    修改相对路径

---
+++ docs/precache-manifest.c5340961160730af70e86a78359b07d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a93b63ef1f71fcaefd6",
+    "url": "./static/css/main.389c49ca.chunk.css"
+  },
+  {
+    "revision": "2a93b63ef1f71fcaefd6",
+    "url": "./static/js/main.718b28f3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a81f7834637c4bd6b13d",
+    "url": "./static/js/2.a4418e97.chunk.js"
+  },
+  {
+    "revision": "a7858d50966a5af7bcecb38c18003f6b",
+    "url": "./index.html"
+  }
+];
